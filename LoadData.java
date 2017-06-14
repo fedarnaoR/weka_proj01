192317ae8ee085819a43a8276d12d83be10e4106
Federico M. Arnao Martin <fedarnao@yahoo.es>
2017-06-15 01:58:25 +0200

    Test for ignoring .class files

--- LoadData.java
+++ LoadData.java
@@ -3,6 +3,7 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 public class LoadData {
 	public static void main(String[] args) throws Exception {
+		//TODO Bajar el fichero .arff
 		DataSource ds = new DataSource("dataset/titanic.arff");
 		Instances data = ds.getDataSet();
 		System.out.println(data.numInstances() + " instances loaded.");
